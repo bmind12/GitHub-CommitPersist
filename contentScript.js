3b358ec1abc020b71a8fd04736894c07ec3b0b57
Sergej R <ryabovsergey12@gmail.com>
2024-03-14 16:22:58 +0100

    fix: make it work with lazily loaded files

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,28 @@
+let store = {
+  commit: null,
+};
+
 chrome.runtime.sendMessage({ message: "getCurrentUrl" }, function (response) {
   if (!response || !response.url) return;
 
   const commit = response?.url?.split("/").pop();
-  restoreFileStates(commit);
+  store = {
+    ...store,
+    commit,
+  };
 
-  const buttons = document.querySelectorAll(".file-header .js-details-target");
-
-  if (!buttons || buttons.length === 0) return;
-
-  addListeners(buttons, commit);
+  restoreFileStates();
+  addListeners(getButtons(document));
+  initObserver();
 });
 
-function restoreFileStates(commit) {
-  chrome.storage.session.get(commit, (storage) => {
-    const state = storage[commit];
+function restoreFileStates(node = document) {
+  chrome.storage.session.get(store.commit, (storage) => {
+    const state = storage[store.commit];
 
     if (!state) return;
 
-    const elements = document.querySelectorAll(
+    const elements = node.querySelectorAll(
       '[data-details-container-group="file"]'
     );
 
@@ -37,16 +42,33 @@ function restoreFileStates(commit) {
   });
 }
 
-function addListeners(buttons, commit) {
+function initObserver() {
+  const targetNode = document.getElementById("files");
+  const config = { childList: true, subtree: true };
+  const callback = (mutations) => {
+    for (const mutation of mutations) {
+      const newNode = mutation.target;
+      addListeners(getButtons(newNode));
+      restoreFileStates(newNode);
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+
+  observer.observe(targetNode, config);
+}
+
+function addListeners(buttons) {
   buttons.forEach((button) => {
-    button.addEventListener("click", (event) => eventHandler(event, commit));
+    button.addEventListener("click", (event) => eventHandler(event));
   });
 }
 
-function eventHandler(event, commit) {
+function eventHandler(event) {
   const button = event.currentTarget;
-  chrome.storage.session.get(commit, (storage = {}) => {
+  chrome.storage.session.get(store.commit, (storage = {}) => {
     const container = button.closest(".js-details-container");
+    const { commit } = store;
 
     if (!commit || !container) return;
 
@@ -68,4 +90,12 @@ function eventHandler(event, commit) {
 
     chrome.storage.session.set(storage);
   });
+}
+
+function getButtons(node) {
+  const buttons = node.querySelectorAll(".file-header .js-details-target");
+
+  if (!buttons || buttons.length === 0) return null;
+
+  return buttons;
 }
